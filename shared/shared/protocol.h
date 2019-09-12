dfd78860c50cd4b369123e0979fc0d09f313f024
Brian Stremel <brianstremel@gmail.com>
2019-09-12 18:11:01 -0300

    Se agrega protocolo de suse y se agrega la firma de recv_message a protocol.h

--- shared/shared/protocol.h
+++ shared/shared/protocol.h
@@ -15,9 +15,12 @@
 
 
 typedef enum{
-	HOLA,
-	ERROR_RECV,
-	NO_CONNECTION
+	SUSE_INIT = 0,
+	SUSE_CREATE = 1,
+	SUSE_SCHEDULE_NEXT = 2,
+	SUSE_WAIT = 3,
+	SUSE_SIGNAL = 4,
+	SUSE_JOIN = 5
 }t_header;
 
 typedef struct{
@@ -29,6 +32,8 @@ typedef struct{
 t_message* create_t_message(t_header head, size_t size, void* content);
 
 int send_message(int socket, t_header head, void* content, size_t size);
+
+t_message* recv_message(int socket);
 
 void free_t_message(t_message* message);
 
