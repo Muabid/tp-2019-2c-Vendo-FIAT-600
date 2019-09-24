f06572926dd914cf13e5f7abfcd84246159d01ad
Brian Stremel <brianstremel@gmail.com>
2019-09-24 17:07:41 -0300

    Se termina connection handler y se prueba con netcat

--- suse/suse.c
+++ suse/suse.c
@@ -22,44 +22,13 @@ int listen_port = 10500;//int listen_port;
 //	config_destroy(archivoConfiguracion);
 //}
 
-void *connection_handler(void *socket_desc)
-{
-    //Get the socket descriptor
-    int sock = *(int*)socket_desc;
-    int read_size;
-    char *message , client_message[2000];
+void *connection_handler(void *socket_desc) {
+	int sock = *(int*)socket_desc;
 
-    //Send some messages to the client
-    message = "Greetings! I am your connection handler\n";
-    write(sock , message , strlen(message));
+	printf("Hola, me crearon\n");
 
-    message = "Now type something and i shall repeat what you type \n";
-    write(sock , message , strlen(message));
 
-    //Receive a message from client
-    while( (read_size = recv(sock , client_message , 2000 , 0)) > 0 )
-    {
-        //end of string marker
-		client_message[read_size] = '\0';
-
-		//Send the message back to client
-        write(sock , client_message , strlen(client_message));
-
-		//clear the message buffer
-		memset(client_message, 0, 2000);
-    }
-
-    if(read_size == 0)
-    {
-        puts("Client disconnected");
-        fflush(stdout);
-    }
-    else if(read_size == -1)
-    {
-        perror("recv failed");
-    }
-
-    return 0;
+	return 0;
 }
 
 int main() {
@@ -71,30 +40,18 @@ int main() {
 
 	int servidor = iniciarServidor(listen_port);
 
-	while((socketCliente = accept(servidor, (void*) &direccionCliente, &tamanioDireccion)))
-	    {
-	        printf("Connection accepted");
-
-	        if( pthread_create( &threadId , NULL ,  connection_handler , (void*) &socketCliente) < 0)
-	        {
-	            perror("could not create thread");
-	            return 1;
-	        }
-
-	        //Now join the thread , so that we dont terminate before the thread
-	        //pthread_join( thread_id , NULL);
-	        puts("Handler assigned");
-	    }
-
-	    if (socketCliente < 0)
-	    {
-	        perror("accept failed");
-	        return 1;
-	    }
-
-
-
-	close(socket);
-
+	while((socketCliente = accept(servidor, (void*) &direccionCliente, &tamanioDireccion))) {
+		printf("Connection accepted");
+		if( pthread_create(&threadId , NULL , connection_handler, (void*) &socketCliente) < 0) {
+			perror("could not create thread");
+			return 1;
+		}
+		pthread_join(threadId , NULL);
+		puts("Handler assigned");
+	}
+	if (socketCliente < 0) {
+		perror("accept failed");
+		return 1;
+	}
 
 }
--- suse/suse.h
+++ suse/suse.h
@@ -5,39 +5,14 @@
 #include <commons/collections/list.h>
 #include <commons/collections/queue.h>
 #include <net.h>
+#include <protocol.h>
 
 #ifndef SUSE_H_
 #define SUSE_H_
 
 
 //FUNCIONES DE NET.H
-	int init_server(int port){
-	int  socket, val = 1;
-	struct sockaddr_in servaddr;
 
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_addr.s_addr =INADDR_ANY;
-	servaddr.sin_port = htons(port);
-
-	socket = create_socket();
-	if (socket < 0) {
-		perror("socket");
-		return EXIT_FAILURE;
-	}
-
-	setsockopt(socket, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
-
-	if (bind(socket,(struct sockaddr*) &servaddr, sizeof(servaddr)) < 0) {
-		return EXIT_FAILURE;
-	}
-
-	if (listen(socket, MAX_CLIENTS)< 0) {
-		return EXIT_FAILURE;
-	}
-
-	return socket;
-
-}
 
 //FIN FUNCIONES NET.H
 
