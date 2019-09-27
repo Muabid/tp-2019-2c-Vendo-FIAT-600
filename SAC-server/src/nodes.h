715db267146b71137562da033a81e4a74993a517
Bruno <bruno2698@gmail.com>
2019-09-26 23:22:13 -0300

    Agrego search_and_test_first_free_block

--- SAC-server/src/nodes.h
+++ SAC-server/src/nodes.h
@@ -8,6 +8,14 @@
 #ifndef NODES_H_
 #define NODES_H_
 
+
+#include "stdint.h"
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <commons/bitarray.h>
+#include <commons/string.h>
+
 #define BLOCK_SIZE 4096
 #define HEADER_BLOCKS 1
 #define BLOCKS_NODE 1024
@@ -15,12 +23,7 @@
 #define BLOCKS_TOTAL FILESYSTEM_SIZE/BLOCK_SIZE
 #define BLOCKS_BITMAP BLOCKS_TOTAL/8
 #define BLOCKS_DATA BLOCKS_TOTAL -1 -BLOCKS_BITMAP - 1024
-#include "stdint.h"
-#include <stdlib.h>
-#include <string.h>
-#include <commons/bitarray.h>
-#include <commons/string.h>
-
+#define T_FILE 2
 
 typedef struct{
 	unsigned char identifier[3];
@@ -40,12 +43,15 @@ typedef struct{
 	int32_t blocks_ptr[1000];
 }GFile ;
 
-t_bitarray* bitmap;
-GFile nodes_table[1024];
 
 typedef struct{
 	char data[4096];
 }t_block;
+
+t_bitarray* bitmap;
+GFile nodes_table[1024];
+
+pthread_mutex_t bitarray_mutex;
 
 GFile create_GFile(char status,char file_name[71],
 		int32_t root,int32_t size,char creation_date[8],
@@ -62,4 +68,5 @@ char* get_name(const char* path);
 char* get_directory(const char* path);
 int search_first_free_node();
 int search_first_free_block();
+int search_and_test_first_free_block();
 #endif /* NODES_H_ */
