088e7c51125b12fdafab0c4c9262254346f3851f
Muabid <bruno2698@gmail.com>
2019-10-20 18:08:56 -0300

    Fix problemas compilación

--- SAC-cli/src/SAC-cli.c
+++ SAC-cli/src/SAC-cli.c
@@ -5,12 +5,13 @@
  *      Author: utnso
  */
 
-#include "protocol.h"
+#include <shared/protocol.h>
 #include "operations_client.h"
-#include "net.h"
+#include <shared/net.h>
+#include <stdint.h>
 
 //Variable global para la conexión con el SAC-server
-int socket;
+int sock;
 
 
 /*
@@ -41,15 +42,57 @@ struct t_runtime_options {
 
 static int do_getattr(const char *path, struct stat *st) {
 	void *buffer = malloc(strlen(path));
+	int32_t size,hardlinks,file_name_len;
+	uint64_t creation_date, modification_date;
+	char* file_name;
+	char status;
+
 	memcpy(buffer, path, strlen(path));
 
-	int operation_response = send_message(socket, GET_ATTR, buffer,
+	int operation_response = send_message(sock, GET_ATTR, buffer,
 			strlen(path));
 
 	free(buffer);
 
 	if (operation_response >= 0) {
-		t_message* message = recv_message(socket); // Acá me va a responder el server algo,
+		t_message* message = recv_message(sock); // Acá me va a responder el server algo,
+		void* content = message->content;
+		memcpy(&size,content,sizeof(int32_t));
+		content = content + sizeof(int32_t);
+
+		memcpy(&creation_date,content,sizeof(int32_t));
+		content = content + sizeof(uint64_t);
+
+		memcpy(&modification_date,content, sizeof(uint64_t));
+		content = content + sizeof(uint64_t);
+
+		memcpy(&status,content,sizeof(char));
+		content = content + sizeof(char);
+
+		memcpy(&hardlinks,content,sizeof(int32_t));
+		content += sizeof(int32_t);
+
+		memcpy(&file_name_len,content,sizeof(int32_t));
+		content += sizeof(int32_t);
+		file_name = malloc(file_name_len + 1);
+
+		memcpy(file_name,content,file_name_len);
+		file_name[file_name_len] = '\0';
+
+		printf("%s | %d | %i | %i | %i | %c",file_name,size,
+				creation_date,modification_date,hardlinks,status);
+
+		st->st_size = size;
+		st->st_nlink = hardlinks;
+		st->st_mtim.tv_sec = modification_date;
+		st->st_ctim.tv_sec = creation_date;
+		st->st_atim.tv_sec = time(NULL);
+
+		if(status == 2){
+			st->st_mode = S_IFDIR | 0777;
+		} else{
+			st->st_mode = S_IFREG | 0777;
+		}
 
 		return 0; // 0 for success
 	} else {
@@ -64,13 +107,13 @@ static int do_readLink(const char *path, char *buf, size_t len) {
 	memcpy(buffer, path, strlen(path));
 	memcpy(buffer + strlen(path), buf, sizeof(buf));
 
-	int operation_response = send_message(socket, READ_LINK, buffer,
+	int operation_response = send_message(sock, READ_LINK, buffer,
 			sizeof(buffer));
 
 	free(buffer);
 
 	if (operation_response >= 0) {
-		t_message* message = recv_message(socket);
+		t_message* message = recv_message(sock);
 
 		return 0;
 
@@ -84,13 +127,13 @@ static int do_create(const char *path, mode_t mode, struct fuse_file_info *fi) {
 	memcpy(buffer, path, strlen(path));
 	memcpy(buffer + strlen(path), mode, sizeof(mode));
 
-	int operation_response = send_message(socket, CREATE, buffer,
+	int operation_response = send_message(sock, CREATE, buffer,
 			sizeof(buffer));
 
 	free(buffer);
 
 	if (operation_response >= 0) {
-		t_message *message = recv_message(socket);
+		t_message *message = recv_message(sock);
 		return 0;
 	} else {
 		return 1;
@@ -98,35 +141,35 @@ static int do_create(const char *path, mode_t mode, struct fuse_file_info *fi) {
 }
 
 static int do_read(const char *path, char *buf, size_t size, off_t off,
-		struct fuese_file_info *fi) {
+		struct fuse_file_info *fi){
 	void *buffer = malloc(strlen(path) + sizeof(buf) + sizeof(size));
-	memcpy(buffer, path, strlen(path));
-	memcpy(buffer + strlen(path), buf, sizeof(buf));
-	memcpy(buffer + strlen(path) + sizeof(buf), size, sizeof(size));
+		memcpy(buffer, path, strlen(path));
+		memcpy(buffer + strlen(path), buf, sizeof(buf));
+		memcpy(buffer + strlen(path) + sizeof(buf), size, sizeof(size));
 
-	int operation_response = send_message(socket, READ, buffer, sizeof(buffer));
+		int operation_response = send_message(sock, READ, buffer, sizeof(buffer));
 
-	free(buffer);
+		free(buffer);
 
-	if (operation_response >= 0) {
-		t_message *message = recv_message(socket);
-		return 0;
-	} else {
-		return 1;
-	}
+		if (operation_response >= 0) {
+			t_message *message = recv_message(sock);
+			return 0;
+		} else {
+			return 1;
+		}
 }
 
 static int do_unlink(const char *path) {
 	void *buffer = malloc(strlen(path));
 	memcpy(buffer, path, strlen(path));
 
-	int operation_response = send_message(socket, UNLINK, buffer,
+	int operation_response = send_message(sock, UNLINK, buffer,
 			sizeof(buffer));
 
 	free(buffer);
 
 	if (operation_response >= 0) {
-		t_message *message = recv_message(socket);
+		t_message *message = recv_message(sock);
 		return 0;
 	} else {
 		return 1;
@@ -137,13 +180,13 @@ static int do_mkdir(const char *path, mode_t mode) {
 	void *buffer = malloc(strlen(path) + sizeof(mode));
 	memcpy(buffer, path, strlen(path));
 
-	int operation_response = send_message(socket, MKDIR, buffer,
+	int operation_response = send_message(sock, MKDIR, buffer,
 			sizeof(buffer));
 
 	free(buffer);
 
 	if (operation_response >= 0) {
-		t_message *message = recv_message(socket);
+		t_message *message = recv_message(sock);
 		return 0;
 	} else {
 		return 1;
@@ -154,13 +197,13 @@ static int do_opendir(const char *path, struct fuse_file_info *fi) {
 	void *buffer = malloc(strlen(path));
 	memcpy(buffer, path, strlen(path));
 
-	int operation_response = send_message(socket, OPENDIR, buffer,
+	int operation_response = send_message(sock, OPENDIR, buffer,
 			sizeof(buffer));
 
 	free(buffer);
 
 	if (operation_response >= 0) {
-		t_message *message = recv_message(socket);
+		t_message *message = recv_message(sock);
 		return 0;
 	} else {
 		return 1;
@@ -171,13 +214,13 @@ static int do_rmdir(const char *path) {
 	void *buffer = malloc(strlen(path));
 	memcpy(buffer, path, strlen(path));
 
-	int operation_response = send_message(socket, RMDIR, buffer,
+	int operation_response = send_message(sock, RMDIR, buffer,
 			sizeof(buffer));
 
 	free(buffer);
 
 	if (operation_response >= 0) {
-		t_message *message = recv_message(socket);
+		t_message *message = recv_message(sock);
 		return 0;
 	} else {
 		return 1;
@@ -191,13 +234,13 @@ static int do_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 	memcpy(buffer, path, strlen(path));
 	memcpy(buffer + strlen(path), buf, sizeof(buf));
 
-	int operation_response = send_message(socket, READDIR, buffer,
+	int operation_response = send_message(sock, READDIR, buffer,
 			sizeof(buffer));
 
 	free(buffer);
 
 	if (operation_response >= 0) {
-		t_message *message = recv_message(socket);
+		t_message *message = recv_message(sock);
 		return 0;
 	} else {
 		return 1;
@@ -210,13 +253,13 @@ static int do_mknod(const char *path, mode_t mode, dev_t rdev) {
 	memcpy(buffer, path, strlen(path));
 	memcpy(buffer + strlen(path), mode, sizeof(mode));
 
-	int operation_response = send_message(socket, MKNODE, buffer,
+	int operation_response = send_message(sock, MKNODE, buffer,
 			sizeof(buffer));
 
 	free(buffer);
 
 	if (operation_response >= 0) {
-		t_message *message = recv_message(socket);
+		t_message *message = recv_message(sock);
 		return 0;
 	} else {
 		return 1;
@@ -288,10 +331,9 @@ int main(int argc, const char* argv[]) {
 			return EXIT_FAILURE;
 		}
 
-	socket = connect_to_server("LOCALHOST", 8080, NULL); // Se establece la conexión
+	sock = connect_to_server("LOCALHOST", 8080, NULL); // Se establece la conexión
 
-	int communication_response = send_message(socket, HI_PLEASE_BE_MY_FRIEND,
-			"HI");
+	int communication_response = send_message(sock, HI_PLEASE_BE_MY_FRIEND,"HI",2);
 
 	if (communication_response >= 0) { // Comunicación ok.
 		/* Cuando la comunicación ya se estableció, tengo que hacer una espera activa para las operaciones del FS? o no es necesario?
