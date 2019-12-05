3effb7a3060473c811e090a08838b287aaa58dca
francisco-aguilar <raguilar@est.frba.utn.edu.ar>
2019-12-05 00:01:06 -0300

    agrego map, sync y unmap

--- muse/src/muse.c
+++ muse/src/muse.c
@@ -95,3 +95,135 @@ void inicializarSemaforos(){
 	pthread_mutex_init(&mut_listaProgramas,NULL);
 	pthread_mutex_init(&mut_bitmap,NULL);
 }
+
+int muse_map(char* id, char* path, uint32_t length, uint32_t flag){
+	int tamanioAMapear = techo(length) * TAMANIO_PAGINA;
+	pthread_mutex_lock(&mut_espacioDisponible);
+	if(espacioDisponible >= tamanioAMapear){
+		espacioDisponible -= tamanioAMapear;
+		pthread_mutex_unlock(&mut_espacioDisponible);
+		t_list* listaSegmentos = obtenerListaSegmentosPorId(id);
+		if(listaSegmentos == NULL){
+			log_info(logger,"F por el programa %s que no existe",id);
+			return -1;
+		}
+		Segmento* segmentoNuevo = malloc(sizeof(Segmento));
+		segmentoNuevo->num_segmento = listaSegmentos->elements_count;
+		segmentoNuevo->es_mmap = true;
+		segmentoNuevo->tamanio_mapeo = length;
+		segmentoNuevo->base_logica = base_logica_segmento_nuevo(listaSegmentos);
+		//segmentoNuevo->info_heaps = NULL;
+		//segmentoNuevo->paginas_liberadas=0;
+		segmentoNuevo->path_mapeo = string_duplicate(path);
+		segmentoNuevo->tamanio = tamanioAMapear;
+		if(flag != MAP_PRIVATE){
+			segmentoNuevo->compartido = true;
+			t_list* paginas = obtenerMapeoExistente(path,length);
+			if(paginas != NULL){
+				segmentoNuevo->paginas = paginas;
+				list_add(listaSegmentos,segmentoNuevo);
+				return segmentoNuevo->base_logica;
+			}
+		}
+		else{
+			segmentoNuevo->compartido = false;
+		}
+		//tengo que crear el el mapeo de cero y llenar lista de mapeo
+		t_list* paginas = list_create();
+		int cantidad_de_paginas = techo(length);
+		for(int i = 0; i < cantidad_de_paginas; i++){
+			Pagina* pag = malloc(sizeof(Pagina));
+			pag->num_pagina = i;
+			pag->bit_marco = NULL;
+			pag->presencia = false;
+			pag->bit_swap = NULL;
+			list_add(paginas,pag);
+		}
+		segmentoNuevo->paginas = paginas;
+		list_add(listaSegmentos,segmentoNuevo);
+
+		Mapeo* mapeo = malloc(sizeof(Mapeo));
+		mapeo->contador = 1;
+		mapeo->paginas = paginas;
+		mapeo->path = string_duplicate(path);
+		mapeo->tamanio = length;
+		//mapeo->tamanio_de_pags = segmentoNuevo->tamanio;
+		pthread_mutex_lock(&mut_mapeos);
+		list_add(listaMapeos,mapeo);
+		pthread_mutex_unlock(&mut_mapeos);
+		return segmentoNuevo->base_logica;
+	}
+	else{
+		pthread_mutex_unlock(&mut_espacioDisponible);
+		//no hay lugar
+		return -1;
+	}
+}
+
+int muse_sync(char* id, uint32_t addr, size_t len){
+	t_list* listaSegmentos = obtenerListaSegmentosPorId(id);
+	if(listaSegmentos == NULL || list_is_empty(listaSegmentos)){
+		log_info(logger,"F por el programa %s que no existe o no tiene segmentos",id);
+		return -1;
+	}
+
+	Segmento* segmentoEncontrado = segmentoAlQuePertenece(listaSegmentos,addr);
+	if(segmentoEncontrado == NULL){
+		log_info(logger,"F por el programa %s que no encontro el segmento",id);
+		return -1;
+	}
+	int dirAlSegmento = addr - segmentoEncontrado->base_logica;
+		if(segmentoEncontrado->es_mmap){
+			int primerPag = dirAlSegmento / TAMANIO_PAGINA;
+			int ultimaPag = techo((dirAlSegmento + len) / TAMANIO_PAGINA) - 1;
+			int cantidadPags = ultimaPag - primerPag + 1;
+			int tamSync = cantidadPags * TAMANIO_PAGINA;
+
+			void* auxiliar = malloc(tamSync);
+			int puntero = 0;
+
+			paginasMapEnMemoria(dirAlSegmento,len,segmentoEncontrado);
+
+			int i = primerPag;
+			while(i <= ultimaPag){
+				Pagina* pag = list_get(segmentoEncontrado->paginas,i);
+				void* punteroMarco = obtenerPunteroAMarco(pag);
+				memcpy(auxiliar+puntero,punteroMarco,TAMANIO_PAGINA);
+				i++;
+				puntero += TAMANIO_PAGINA;
+				tamSync -= TAMANIO_PAGINA;
+			}
+			int posInicial = primerPag * TAMANIO_PAGINA;
+			FILE* file = fopen(segmentoEncontrado->path_mapeo,"r+");
+			if(file != NULL){
+				if(fseek(file,posInicial,SEEK_SET == 0)){
+					fwrite(auxiliar,cantidadPags*TAMANIO_PAGINA,1,file);
+					free(auxiliar);
+					fclose(file);
+					return 0;
+				}
+			}
+			free(auxiliar);
+		}
+		log_info(logger,"F porque el segmento no es mmap",id); // si llega aca me suicido
+		return -1;
+}
+
+int muse_unmap(char* id, uint32_t dir){
+	t_list* listaSegmentos = obtenerListaSegmentosPorId(id);
+	if(listaSegmentos == NULL){
+		log_info(logger,"F por el programa %s, no se encontró su lista de segmentos", id);
+		return -1;
+	}
+	Segmento* segmentoEncontrado = segmentoAlQuePertenece(listaSegmentos,dir);
+	if(segmentoEncontrado != NULL){
+		if(segmentoEncontrado->es_mmap){
+			unmapear(segmentoEncontrado,id);
+			return 0;
+		}
+	}
+	perror("FFFFFFFF");
+	return -1;
+}
+
+
