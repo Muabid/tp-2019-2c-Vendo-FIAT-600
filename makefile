59896df50b100fcd48340415e87e700074ca4db0
utnso <hernan_rodriguez_2013@hotmail.com>
2019-12-07 11:42:56 -0300

    Make suse entrega

--- makefile
+++ makefile
@@ -1,6 +1,11 @@
-all: 
+all:
+	-cd shared/Debug && make all 
 	-cd SAC-server/Debug && make all
 	-cd SAC-cli/Debug && make all
+	-cd suse/Debug && make all
+	-cd muse/Debug && make all
+	-cd hilolay/Debug && make all
+	-export LD_LIBRARY_PATH=/home/utnso/tp-2019-2c-Vendo-FIAT-600/shared/Debug
 
 
 clean:
--- suse/Debug/src/subdir.mk
+++ suse/Debug/src/subdir.mk
@@ -17,7 +17,7 @@ C_DEPS += \
 src/%.o: ../src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -I"/home/utnso/workspace/tp-2019-2c-Vendo-FIAT-600/shared" -O0 -g3 -Wall -c -fmessage-length=0 -pthread -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	gcc -I"/home/utnso/tp-2019-2c-Vendo-FIAT-600/shared" -O0 -g3 -Wall -c -fmessage-length=0 -pthread -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
